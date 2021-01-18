b5cc854152adf3ea48b4445234ce8a1968c227fd
CFE <hello@teamcfe.com>
2021-01-18 15:39:20 -0800

    New Model Index and Multi-model Elasticsearch Query

---
+++ products/documents.py
@@ -0,0 +1,21 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+
+
+from .models import Product
+
+@registry.register_document
+class ProductDocument(Document):
+    class Index:
+        '''
+        Elasticsearch index
+        '''
+        name='products'
+    url = fields.TextField(attr='get_absolute_url')
+    content = fields.TextField(attr='description')
+    class Django:
+        model = Product
+        fields = [
+            'id',
+            'title',
+        ]
--- search/lookup.py
+++ search/lookup.py
@@ -7,13 +7,30 @@ ELASTIC_HOST_KEY = getattr(settings, 'ELASTIC_HOST_KEY')
 
 client = elasticsearch.Elasticsearch(hosts=[ELASTIC_HOST_KEY])
 
+INDEXES = [
+    'posts',
+    'products'
+]
 
-def perform_lookup(query, index='posts', fields = ['title', 'content']):
+
+def perform_lookup(query, index=INDEXES, fields = ['title', 'content']):
     if not query:
         return
-    results = Search(
+    search_results = Search(
         index=index).using(client).query("multi_match", 
         fields=fields,  fuzziness='AUTO', query=query)
-    for hit in results:
+    results = []
+    for hit in search_results:
         print(hit.id)
         print(hit.title)
+        print(hit.meta.index)
+        print(hit.meta.score)
+        data = {
+            "id": hit.id,
+            "title": hit.title,
+            "index": hit.meta.index,
+            "content": hit.content,
+            "url": hit.url,
+        }
+        results.append(data)
+    return results
