2eb805968232822d3849a83d6b54e46f95f768f2
CFE <hello@teamcfe.com>
2021-01-22 13:25:13 -0800

    20 - Production Ready Environment Variables with django-dotenv

--- elastic_django/settings.py
+++ elastic_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7zmtlm09k8xl001znbbg+=s!rzyg-l+^!q(74020$%*qebu27g'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
